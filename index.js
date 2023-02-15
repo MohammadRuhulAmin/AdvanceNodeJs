b1ab1d6a2bb5ed4ec83a86edf261c86ed9a54403
ruhul <ra.01403288711@gmail.com>
2023-02-15 11:20:07 +0600

    model added

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import models from './models/index.js';
+
+
 const app = express();
 const port = 3000;
+app.use(express.json());
 
+const log = (msg) =>console.log(msg);
 
+app.get('/',(req,res)=>{
+    res.send('Hello world ' + req.query.id);
+});
 
+app.post('/',(req,res)=>{
+    const body = req.body;
 
+    res.send('This is post request ' + body.message);
+})
 
 app.listen(port , ()=>{
     console.log(`Listening to port No ${port}`);
 });
+
+log(models)
