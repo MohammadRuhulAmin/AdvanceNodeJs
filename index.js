641333075b82c041dce935b8791a931f913bf2e5
ruhul <ra.01403288711@gmail.com>
2023-02-15 23:10:24 +0600

    Mongodb server connected successfully

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import models from './models/index.js';
-
+import mongoose  from "mongoose";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 const log = (msg) =>console.log(msg);
+
+const uri = "mongodb://127.0.0.1:27017/parcelkoi";
+const options = {};
+mongoose.set('strictQuery', true);
+const connectWithDb = ()=>{
+    mongoose.connect(uri,options,(err,db)=>{
+        if(err)console.error(err);
+        else{
+            log("Database connection established");
+        }
+    })
+}
+
+connectWithDb();
 
 app.get('/',(req,res)=>{
     res.send('Hello world ' + req.query.id);
