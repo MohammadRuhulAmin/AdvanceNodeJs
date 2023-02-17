08f17ac4b836de180f82cade30a3fdc96eded64a
ruhul <ra.01403288711@gmail.com>
2023-02-17 10:35:14 +0600

    custom corrolation id function generator established

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import configure from "./controllers";
 import { handleErrors } from "./middlewares/handleErrors";
+import processRequest from "./middlewares/processRequest";
 
 import connectWithDb from "./mongo";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-
-
+app.use(processRequest);
 
 connectWithDb();
 
